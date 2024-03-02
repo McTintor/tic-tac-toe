1277526c95c1022a7a961ecec098c4a4bac74389
McTintor <igortintor99@gmail.com>
2024-03-02 21:41:22 +0100

    idunno

--- script.js
+++ script.js
@@ -9,6 +9,51 @@ const winningCombinations = [
     [6, 7, 8]
 ];
 
-function Player(name, sign, turn) {
+function createPlayer(name, sign) {
+    let turn = false; // Initially, it's not this player's turn
+    let score = 0; // Initial score is 0
     
+    function incrementScore() {
+      score++;
+    }
+  
+    return {
+      name: name,
+      sign: sign,
+      getTurn: function() {
+        return turn;
+      },
+      setTurn: function(newTurn) {
+        turn = newTurn;
+      },
+      getScore: function() {
+        return score;
+      },
+      incrementScore: incrementScore
+    };
+  }
+  
+function playGame() {
+    const player1 = createPlayer('Player 1', 'X');
+    const player2 = createPlayer('Player 2', 'O');
+    let currentPlayer = player1;
+
+    const board = [
+        '', '', '',
+        '', '', '',
+        '', '', '',
+    ];
+
+    makeMove(currentPlayer);
+    switchTurn(currentPlayer);
+}
+
+function switchTurn(currentPlayer) {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+
+function makeMove(currentPlayer) {
+    if (currentPlayer === player1) {
+
+    }
 }
